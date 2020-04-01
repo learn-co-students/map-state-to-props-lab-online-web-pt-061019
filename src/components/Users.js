3aa11ee849f129ebcb30a96762fe2c35bd78e693
Khris Punzalan <khrispunzalan@gmail.com>
2020-04-01 16:20:49 -0700

    Done.

--- src/components/Users.js
+++ src/components/Users.js
@@ -1,21 +1,29 @@
 import React, { Component } from 'react';
 // add any needed imports here
-class Users extends Component {
+import { connect } from 'react-redux';
 
+class Users extends Component {
   render() {
     return (
       <div>
         <ul>
-          Users!
+          Users:
           {/* Write code here that displays the usernames of all users in the Redux store */}
+          {this.props.users.map((u, idx) => (
+            <li key={idx}> {u.username}</li>
+          ))}
           {/* In addition, display the total number of users curently in the store */}
         </ul>
+        <p>Total Users: {this.props.userCount}</p>
       </div>
-    )
+    );
   }
 }
 
 //add mapStateToProps here
-
+const mapStateToProps = (state) => {
+  return { users: state.users, userCount: state.users.length };
+};
 // connect this component to Redux
-export default Users
+// export default Users
+export default connect(mapStateToProps)(Users);
--- src/reducers/manageUsers.js
+++ src/reducers/manageUsers.js
@@ -1,15 +1,22 @@
-export default function manageUsers(state = {
-  users: [],
-}, action){
+export default function manageUsers(
+  state = {
+    users: [],
+  },
+  action,
+) {
   switch (action.type) {
     case 'ADD_USER':
       console.log('adding ', action.user);
       return {
         ...state,
-        users: [...state.users, action.user]
-      }
+        users: [...state.users, action.user],
+      };
+    // case 'GET_COUNT_OF_USERS':
+    //   return Object.assign({}, state, {
+    //     users: state.users.concat(state.users.length + 1),
+    //   });
 
     default:
       return state;
   }
-};
+}
